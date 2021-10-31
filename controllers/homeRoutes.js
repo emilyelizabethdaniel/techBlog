e9ee9be39842a35e403ff6bb773ad56173095a02
Basquiat <ememdaniel98@gmail.com>
2021-10-30 19:12:49 -0500

    got homepage route working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,21 +2,26 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
 
 router.get('/', async(req, res) => {
+    console.log('am i broken');
     try {
-        // Get all projects and JOIN with user data
-        const postData = await Post.findAll({
-            include: [{
-                model: Post,
-                attributes: ['title'],
-            }, ],
-        });
+        // var projectData = await Project.findAll();
+        // var projectData = [];
+        // projectData.forEach(projObj => {
 
-        // Serialize data so the template can read it
-        const posts = postData.map((post) => post.get({ plain: true }));
-
-        // Pass serialized data and session flag into template
-        res.render('homepage', { posts });
+        //     projectData.push(projObj.get({ plain: true }))
+        // })
+        // res.render('homepage', { project: projectData })
+        // same code using mapping
+        const postData = await Post.findAll();
+        const mappedData = postData.map(postObj => postObj.get({ plain: true }))
+            // const project = projObj.get({ plain: true });
+        console.log(mappedData);
+        res.render('homepage', { mappedData });
     } catch (err) {
+        console.log(err);
         res.status(500).json(err);
-    }
+    };
+
 });
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
-//require in all routes
-
 const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
+
 module.exports = router;
