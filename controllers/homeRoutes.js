ccce8c80e551aa696755df1250571f5102fa9b10
Basquiat <ememdaniel98@gmail.com>
2021-10-31 19:16:31 -0500

    added login and sign up handlebars pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -37,4 +37,20 @@ router.get('/post/:id', async(req, res) => {
 });
 
 
+router.get('/login', async(req, res) => {
+    try {
+        res.render('login');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get('/signup', async(req, res) => {
+    try {
+        res.render('signup');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
 module.exports = router;
