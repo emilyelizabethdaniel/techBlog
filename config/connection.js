c4073a84dfe6cd173eedb888a5e5e5a6b000d86d
Basquiat <ememdaniel98@gmail.com>
2021-11-02 19:35:00 -0500

    added changes to project to link to heroku

--- config/connection.js
+++ config/connection.js
@@ -6,14 +6,13 @@ require('dotenv').config();
 // const models = require('../models/index')
 
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD, {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306,
-    }
-);
-
+const sequelize = process.env.JAWSDB_URL ?
+    new Sequelize(process.env.JAWSDB_URL) :
+    new Sequelize(process.env.DB_NAME,
+        process.env.DB_USER, process.env.DB_PW, {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306
+        }
+    );
 module.exports = sequelize;
